b86f5fd2f7c92bbdb8c905a9af9cc1648c9d8025
Fırat <113585221+firatgurler10@users.noreply.github.com>
2024-09-11 10:48:53 +0300

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // JavaScript ve TypeScript dosyaları
+    "./public/index.html", // HTML dosyaları
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
